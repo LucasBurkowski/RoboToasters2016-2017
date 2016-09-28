4214f48d73b0ef130c67ae050388bd3d327ace21
Solfatari <jacosta63@ford.com>
2016-09-28 15:15:05 -0400

    initial commit for vuforia testing branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
+/**
+ * Created by jacost63 on 9/28/2016.
+ */
+@TeleOp(name="VuforiaTest", group="Linear Opmode")
+public class VuforiaTest extends LinearOpMode {
+
+    VuforiaLocalizer vf;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+}
