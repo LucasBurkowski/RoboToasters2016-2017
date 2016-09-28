58a2446326fb560aa0e3a6b1331c5b2f4e18dfda
Chris Acosta <jacosta63@ford.com>
2016-09-28 09:47:45 -0400

    fixed the number of motors used for the wheels.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTestFire.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTestFire.java
@@ -11,23 +11,33 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
  */
 @Autonomous(name="AutonomousTestFire", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
 public class AutonomousTestFire extends OpMode {
+    private static final int ENCODER_PULSES_PER_REV = 280;
+
     private static final int ZERO_POWER = 0;
     private static final int MAX_FORWARD = 1;
     private static final int MAX_REVERSE = -1;
 
-    DcMotor motorLeft = null;
-    DcMotor motorRight = null;
+    private DcMotor motorLeft1 = null;
+    private DcMotor motorLeft2 = null;
+    private DcMotor motorRight1 = null;
+    private DcMotor motorRight2 = null;
 
     @Override
     public void init(){
-        this.motorLeft = this.hardwareMap.dcMotor.get("motorLeft");
-        this.motorRight = this.hardwareMap.dcMotor.get("motorRight");
+        this.motorLeft1 = this.hardwareMap.dcMotor.get("motorLeft1");
+        this.motorLeft2 = this.hardwareMap.dcMotor.get("motorLeft2");
+        this.motorRight1 = this.hardwareMap.dcMotor.get("motorRight1");
+        this.motorRight2 = this.hardwareMap.dcMotor.get("motorRight2");
         DcMotor.RunMode mode = DcMotor.RunMode.RUN_USING_ENCODER;
-        this.motorLeft.setMode(mode);
-        this.motorRight.setMode(mode);
+        this.motorLeft1.setMode(mode);
+        this.motorLeft2.setMode(mode);
+        this.motorRight1.setMode(mode);
+        this.motorRight2.setMode(mode);
 
-        this.motorLeft.setDirection(DcMotor.Direction.FORWARD);
-        this.motorRight.setDirection(DcMotor.Direction.REVERSE);
+        this.motorLeft1.setDirection(DcMotor.Direction.FORWARD);
+        this.motorLeft2.setDirection(DcMotor.Direction.FORWARD);
+        this.motorRight1.setDirection(DcMotor.Direction.REVERSE);
+        this.motorRight2.setDirection(DcMotor.Direction.REVERSE);
 
         AllStop();
     }
@@ -55,23 +65,33 @@ public class AutonomousTestFire extends OpMode {
         while(msSleep < System.currentTimeMillis()){}
     }
     private void MaxForward(){
-        this.motorRight.setPower(MAX_FORWARD);
-        this.motorLeft.setPower(MAX_FORWARD);
+        this.motorRight1.setPower(MAX_FORWARD);
+        this.motorRight2.setPower(MAX_FORWARD);
+        this.motorLeft1.setPower(MAX_FORWARD);
+        this.motorLeft2.setPower(MAX_FORWARD);
     }
     private void MaxReverse(){
-        this.motorRight.setPower(MAX_REVERSE);
-        this.motorLeft.setPower(MAX_REVERSE);
+        this.motorRight1.setPower(MAX_REVERSE);
+        this.motorRight2.setPower(MAX_REVERSE);
+        this.motorLeft1.setPower(MAX_REVERSE);
+        this.motorLeft2.setPower(MAX_REVERSE);
     }
     private void HardRight(){
-        this.motorRight.setPower(MAX_REVERSE);
-        this.motorLeft.setPower(MAX_FORWARD);
+        this.motorRight1.setPower(MAX_REVERSE);
+        this.motorRight2.setPower(MAX_REVERSE);
+        this.motorLeft1.setPower(MAX_FORWARD);
+        this.motorLeft2.setPower(MAX_FORWARD);
     }
     private void HardLeft(){
-        this.motorRight.setPower(MAX_FORWARD);
-        this.motorLeft.setPower(MAX_REVERSE);
+        this.motorRight1.setPower(MAX_FORWARD);
+        this.motorRight2.setPower(MAX_FORWARD);
+        this.motorLeft1.setPower(MAX_REVERSE);
+        this.motorLeft2.setPower(MAX_REVERSE);
     }
     private void AllStop(){
-        this.motorLeft.setPower(ZERO_POWER);
-        this.motorRight.setPower(ZERO_POWER);
+        this.motorLeft1.setPower(ZERO_POWER);
+        this.motorLeft2.setPower(ZERO_POWER);
+        this.motorRight1.setPower(ZERO_POWER);
+        this.motorRight2.setPower(ZERO_POWER);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -12,22 +12,30 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class TeleOpTest extends OpMode{
 
-    DcMotor motorLeft = null;
-    DcMotor motorRight = null;
+    DcMotor motorLeft1 = null;
+    DcMotor motorLeft2 = null;
+    DcMotor motorRight1 = null;
+    DcMotor motorRight2 = null;
     protected float leftY;
     protected float rightY;
 
 
     @Override
     public void init() {
-        this.motorLeft = this.hardwareMap.dcMotor.get("motorLeft");
-        this.motorRight = this.hardwareMap.dcMotor.get("motorRight");
+        this.motorLeft1 = this.hardwareMap.dcMotor.get("motorLeft1");
+        this.motorLeft2 = this.hardwareMap.dcMotor.get("motorLeft2");
+        this.motorRight1 = this.hardwareMap.dcMotor.get("motorRight1");
+        this.motorRight2 = this.hardwareMap.dcMotor.get("motorRight2");
         DcMotor.RunMode mode = DcMotor.RunMode.RUN_USING_ENCODER;
-        this.motorLeft.setMode(mode);
-        this.motorRight.setMode(mode);
+        this.motorLeft1.setMode(mode);
+        this.motorLeft2.setMode(mode);
+        this.motorRight1.setMode(mode);
+        this.motorRight2.setMode(mode);
 
-        this.motorLeft.setDirection(DcMotor.Direction.FORWARD);
-        this.motorRight.setDirection(DcMotor.Direction.REVERSE);
+        this.motorLeft1.setDirection(DcMotor.Direction.FORWARD);
+        this.motorLeft2.setDirection(DcMotor.Direction.FORWARD);
+        this.motorRight1.setDirection(DcMotor.Direction.REVERSE);
+        this.motorRight2.setDirection(DcMotor.Direction.REVERSE);
     }
 
     @Override
@@ -35,7 +43,9 @@ public class TeleOpTest extends OpMode{
         leftY = -gamepad1.left_stick_y;
         rightY = -gamepad1.right_stick_y;
 
-        motorLeft.setPower(leftY);
-        motorRight.setPower(rightY);
+        motorLeft1.setPower(leftY);
+        motorLeft2.setPower(leftY);
+        motorRight1.setPower(rightY);
+        motorRight2.setPower(rightY);
     }
 }
